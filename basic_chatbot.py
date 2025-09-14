78f933e8af13c28a981a630ec5e5aec8f537b379
rishi singh <rishisingh.singh3@gmail.com>
2025-09-14 17:35:06 +0530

    Add files via upload

--- basic_chatbot.py
+++ basic_chatbot.py
@@ -35,13 +35,13 @@ chatbot = graph.compile(checkpointer=checkpointer)
 
 # final_state = chatbot.invoke(initial_state)
 
-thread_id = 1
-while True:
-    user_message = input("Type Here ...\n")
-    print('User:', user_message)
-    if user_message.strip().lower() in ['exit', 'quit', 'bye']:
-        break
+# thread_id = 1
+# while True:
+#     user_message = input("Type Here ...\n")
+#     print('User:', user_message)
+#     if user_message.strip().lower() in ['exit', 'quit', 'bye']:
+#         break
 
-    config = {'configurable' : {'thread_id' : thread_id}}
-    response = chatbot.invoke({'messages' : [HumanMessage(content=user_message)]}, config=config)
-    print('AI:', response['messages'][-1].content)
+#     config = {'configurable' : {'thread_id' : thread_id}}
+#     response = chatbot.invoke({'messages' : [HumanMessage(content=user_message)]}, config=config)
+#     print('AI:', response['messages'][-1].content)
